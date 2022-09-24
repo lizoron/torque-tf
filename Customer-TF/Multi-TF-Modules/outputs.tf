ff5b20279f2d45d1026f3ea223c4b1f315e61390
Eric Silver <107945446+lizoron@users.noreply.github.com>
2022-09-23 22:32:42 -0500

    Update outputs.tf

--- Customer-TF/Multi-TF-Modules/outputs.tf
+++ Customer-TF/Multi-TF-Modules/outputs.tf
@@ -1,14 +1,3 @@
 output "public_dns" {
   value = aws_instance.myapp.public_dns
 }
-
-output "db_pass" {
-  value = data.aws_ssm_parameter.db_pass.value
-  sensitive = true
-}
-
-output "db_user" {
-  value = data.aws_ssm_parameter.db_user.value
-  sensitive = true
-}
-
