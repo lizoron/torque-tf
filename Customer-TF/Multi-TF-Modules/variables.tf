c9366fc063742c5768523ae02f4e057863d04949
Eric Silver <107945446+lizoron@users.noreply.github.com>
2022-09-23 21:58:18 -0500

    Create variables.tf

---
+++ Customer-TF/Multi-TF-Modules/variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  type    = string
+  default = "My Instance"
+}
+
+variable "db_user" {
+  type    = string
+  sensitive = true
+}
+
+variable "db_pass" {
+  type    = string
+  sensitive = true
+}
+
+variable "s3_bucket" {
+  type    = string
+  default = "my-app-bucket"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-west-2"
+}
