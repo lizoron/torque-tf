8df41c6b9464730b8068509459d66a252e85c34e
Eric Silver <107945446+lizoron@users.noreply.github.com>
2022-09-23 22:33:56 -0500

    Update variables.tf

--- Customer-TF/Multi-TF-Modules/variables.tf
+++ Customer-TF/Multi-TF-Modules/variables.tf
@@ -13,14 +13,6 @@ variable "db_pass" {
   sensitive = true
 }
 
-data "aws_ssm_parameter" "db_pass" {
-  name = var.param_db_pass_name
-}
-
-data "aws_ssm_parameter" "db_user" {
-  name = var.param_db_user_name
-}
-
 variable "s3_bucket" {
   type    = string
   default = "my-app-bucket"
@@ -29,14 +21,4 @@ variable "s3_bucket" {
 variable "aws_region" {
   type    = string
   default = "us-west-2"
-}
-
-variable "param_db_pass_name" {
-  type    = string
-  default = "example_db_password"
-}
-
-variable "param_db_user_name" {
-  type    = string
-  default = "example_db_user"
 }
