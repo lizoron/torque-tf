4b4727a98c7d367c909d4239910d4f90f74c2179
Eric Silver <107945446+lizoron@users.noreply.github.com>
2022-09-23 22:34:34 -0500

    Update variables.tf

--- Customer-TF/Multi-TF-Modules/variables.tf
+++ Customer-TF/Multi-TF-Modules/variables.tf
@@ -3,16 +3,6 @@ variable "name" {
   default = "My Instance"
 }
 
-variable "db_user" {
-  type    = string
-  sensitive = true
-}
-
-variable "db_pass" {
-  type    = string
-  sensitive = true
-}
-
 variable "s3_bucket" {
   type    = string
   default = "my-app-bucket"
