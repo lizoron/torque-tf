d2bd3dd5ef13b08272f23940dd2f82b86e531244
Eric Silver <107945446+lizoron@users.noreply.github.com>
2022-09-23 22:08:45 -0500

    Update and rename multi.tf to instanceandssm.tf

---
+++ Customer-TF/Multi-TF-Modules/instanceandssm.tf
@@ -0,0 +1,33 @@
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  
+data "aws_ssm_parameter" "db_pass" {
+  name = var.param_db_pass_name
+}
+
+data "aws_ssm_parameter" "db_user" {
+  name = var.param_db_user_name
+}
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "myapp" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = var.name
+  }
+}
