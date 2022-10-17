2dc7167050ee5bea11de4b543df7dfd5a866aa40
lizoron <ericsilver20@gmail.com>
2022-10-17 14:45:31 -0500

    adding acl to bucket resource

--- Customer-TF/Multi-TF-Modules/main.tf
+++ Customer-TF/Multi-TF-Modules/main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "myapp" {
   }
 }
 
-resource "aws_s3_bucket" "bucket" {
+resource "aws_s3_bucket_acl" "bucket" {
     bucket = "cs-eric-s-bucket"
     force_destroy = true  
     acl = "private"
