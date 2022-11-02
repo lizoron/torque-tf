066c72684507068284a5340799efe1017cccfb58
lizoron <ericsilver20@gmail.com>
2022-11-02 15:55:45 -0500

    change

--- Restart/Customer-TF/Instance/outputs.tf
+++ Restart/Customer-TF/Instance/outputs.tf
@@ -1,7 +1,9 @@
 
-output "public_dns" {
-  value = aws_instance.myapp.public #something not sure??
+#output "public_dns" {
+#  value = aws_instance.myapp.public #something not sure??
 
   # hint: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+#}
+output "public_dns" {
+  value = aws_instance.myapp.public_dns
 }
-
