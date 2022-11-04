593c8f76553e5fad8cd787949a9b6a3f8e78c588
lizoron <ericsilver20@gmail.com>
2022-11-04 17:58:27 -0500

    commit

---
+++ Restart/Customer-TF - Working Copy/S3-Bucket/s3.tf
@@ -0,0 +1,22 @@
+variable "aws_region" {
+    type    = string
+    default = "us-west-2"
+}
+
+resource "aws_s3_bucket" "bucket" {
+    bucket = "my-bucket-test-1995"
+    force_destroy = true  
+    acl = "public-read-write"
+
+    tags = {
+    Name        = "My bucket"
+    }
+}
+
+output "bucket_name" {
+    value = aws_s3_bucket.bucket.bucket
+}
+
+provider "aws" {
+    region  = var.aws_region
+}
