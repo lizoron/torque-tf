39ae9d356c3e2118fcb913ad09ac18f285209619
lizoron <ericsilver20@gmail.com>
2022-11-07 15:24:13 -0600

    go

--- Restart/Customer-TF - Working Copy/Instance/outputs.tf
+++ Restart/Customer-TF - Working Copy/Instance/outputs.tf
@@ -1,10 +1,4 @@
 
 output "public_dns" {
-  value = aws_instance.myapp.public #something not sure??
-
-  # hint: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+  value = aws_instance.myapp.public_dns
 }
-
-#output "public_dns" {
-#  value = aws_instance.myapp.public_dns
-#}
