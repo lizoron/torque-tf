7405dca397602d3f67d6582330922480a30113d9
lizoron <ericsilver20@gmail.com>
2022-11-07 15:42:08 -0600

    go

---
+++ Restart/Instance/instance.tf
@@ -0,0 +1,36 @@
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "myapp" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = var.name
+  }
+}
+
+resource "aws_s3_bucket" "bucket" {
+    #bucket = "ericsilver-bucket"
+    bucket = "aws_s3_bucket.bucket.id"
+    force_destroy = true  
+    acl = "private"
+
+    tags = {
+    Name        = "My bucket"
+    }
+}
