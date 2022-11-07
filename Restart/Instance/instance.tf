d39890efc080f3ae256fbd753beb34769354c01b
lizoron <ericsilver20@gmail.com>
2022-11-07 16:35:30 -0600

    go

--- Restart/Instance/instance.tf
+++ Restart/Instance/instance.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "myapp" {
 
 resource "aws_s3_bucket" "bucket" {
     #bucket = "ericsilver-bucket"
-    bucket = aws_s3_bucket.bucket_name.id
+    bucket = var.aws_s3_bucket.bucket_name.id
     force_destroy = true  
     acl = "private"
 
