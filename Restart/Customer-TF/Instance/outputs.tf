156d881df5bdb630a2d56af419b8501e4f0ff554
lizoron <ericsilver20@gmail.com>
2022-11-09 10:46:21 -0600

    go

--- Restart/Customer-TF/Instance/outputs.tf
+++ Restart/Customer-TF/Instance/outputs.tf
@@ -1,8 +1,4 @@
 
 output "public_dns" {
-  value = aws_instance.myapp.public 
+  value = aws_instance.myapp.public_dns
 }
-#Step 3
-#output "public_dns" {
-#  value = aws_instance.myapp.public_dns
-#}
