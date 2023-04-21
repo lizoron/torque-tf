16befe3a707192850d3c0fa8285e9984b0584844
lizoron <ericsilver20@gmail.com>
2023-04-21 10:59:28 -0500

    test

--- tf-tetris/main.tf
+++ tf-tetris/main.tf
@@ -88,3 +88,19 @@ resource "aws_s3_object" "webapp_file" {
   content_type = "text/html"
 }
 
+resource "aws_s3_bucket_public_access_block" "access_block" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "ownership_controls" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
